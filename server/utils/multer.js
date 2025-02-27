68a804aa45dbdaa467c9924a1f477a89bbda3b3a
Codex-2005 <shivam.j@technmanconsulting.com>
2025-02-27 16:35:00 -0500

    multer

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,4 +1,11 @@
+// import multer from "multer";
+
+// const upload = multer({ dest: "uploads/" });
+// export default upload;
 import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.memoryStorage(); // Use memory instead of disk
+
+const upload = multer({ storage });
+
 export default upload;
